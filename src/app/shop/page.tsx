86f107162d1b338b1f312f574696ef4ccd4450fc
Nidhinsanju <sanjuronaldo77@gmail.com>
2024-01-26 12:20:41 +0530

    added key prop

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -12,6 +12,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import Products from "@/models/Products";
 
 interface Products {
   Title: String;
@@ -23,7 +24,7 @@ interface Products {
 }
 
 // type ApiResponse = NextResponse<any[]> | NextResponse<{ error: string }>;
-async function Page(ProductID: Number) {
+async function Page() {
   const data = await getData();
 
   return (
@@ -31,13 +32,17 @@ async function Page(ProductID: Number) {
       <main className="flex flex-wrap flex-row gap-8 items-center justify-center my-10">
         {data?.map((data1: Products) => {
           return (
-            <div className=" h-60 bg-white w-1/5 flex flex-row  rounded-xl shadow-gray-400 shadow-sm">
+            <div
+              key={data1.ProductID.toString()}
+              className=" h-60 bg-white w-1/5 flex flex-row  rounded-xl shadow-gray-400 shadow-sm"
+            >
               <div className=" h-full w-2/5 ">
                 <img
                   src={data1.Imagelink}
                   className="  rounded-l-xl h-full w-full "
                 />
               </div>
+
               <div className=" h-full w-2/4 flex flex-col pl-5 items-center justify-center ">
                 <div className=" h-3/5 w-full  flex flex-col justify-center">
                   <h1 className="font-extrabold text-lg font-serif">
@@ -49,9 +54,7 @@ async function Page(ProductID: Number) {
                   </div>
                 </div>
                 <div className=" h-1/5 w-full  ">
-                  <Button >
-                    Buy now{" "}
-                  </Button>
+                  <Button>Buy now </Button>
                 </div>
               </div>
             </div>

dec30f1e8a0df76e799f7834f43563c689db667c
Nidhinsanju <sanjuronaldo77@gmail.com>
2024-02-13 10:28:00 +0530

    user validation

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,5 @@
-import type { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+// import type { NextAuthOptions } from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
 export const options = {
   // providers: [
   //   CredentialsProvider({

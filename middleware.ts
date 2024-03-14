21de677e231f47e5cc5f93353029cf10b1d1a5a5
nidhin sanju <nidhinsanjuk@gmail.com>
2024-03-14 23:03:57 +0530

    Cors erros fixing

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,30 @@
 import { NextResponse, NextRequest } from "next/server";
 
-export default function middleware(req: NextRequest) {
-  console.log("now running");
-  return NextResponse.next();
+// export default function middleware(req: NextRequest) {
+//   console.log("now running");
+//   return NextResponse.next();
+// }
+
+export function middleware() {
+  // retrieve the current response
+  const res = NextResponse.next();
+
+  // add the CORS headers to the response
+  res.headers.append("Access-Control-Allow-Credentials", "true");
+  res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+  res.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,DELETE,PATCH,POST,PUT"
+  );
+  res.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  return res;
 }
+
+// specify the path regex to apply the middleware to
+export const config = {
+  matcher: "/api/login/:path*",
+};

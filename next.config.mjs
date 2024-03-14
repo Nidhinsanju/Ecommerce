4ff3244fc015d6208e7b9de728288ef4c2aca068
nidhin sanju <nidhinsanjuk@gmail.com>
2024-03-14 22:54:17 +0530

    Cors erros fixing

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/login*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
